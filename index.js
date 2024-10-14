d6b2c751b84f8c3b0ca2074bfac974ecbd5395a9
jodahye <everysun@amuz.co.kr>
2024-10-14 19:22:56 +0900

    mqtt server petcare

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const fs = require("fs");
+const { createServer } = require("http");
+const mqtt = require("mqtt");
+const { Server } = require("socket.io");
+
+const options = {
+    port: 8883,
+    protocol: "mqtts",
+    key: fs.readFileSync("assets/data/claim-private.key"),
+    cert: fs.readFileSync("assets/data/claim-cert.pem"),
+    ca: fs.readFileSync("assets/data/root-CA.crt"),
+    rejectUnauthorized: true,
+};
+
+const client = mqtt.connect("mqtts://axjobfp4mqj2j-ats.iot.ap-northeast-2.amazonaws.com", options);
+
+client.on("connect", () => {});
+
+const app = express();
+const corsOptions = {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+const server = createServer(app);
+const io = new Server(server, { cors: corsOptions });
+
+io.on("connection", (socket) => {
+    socket.on("setDeviceId", (data) => {
+        const deviceId = data.deviceId;
+        client.subscribe(`iot/petcarezone/topic/states/${deviceId}`, (err) => {
+            if (err) {
+                console.error("Subscription error:", err);
+            } else {
+                console.log(`Subscribed to iot/petcarezone/topic/states/${deviceId}`);
+            }
+        });
+
+        client.subscribe(`iot/petcarezone/topic/events/${deviceId}`, (err) => {
+            if (err) {
+                console.error("Subscription error:", err);
+            } else {
+                console.log(`Subscribed to iot/petcarezone/topic/events/${deviceId}`);
+            }
+        });
+    });
+
+    client.on("message", (topic, message) => {
+        console.log("Message received");
+        const msg = message.toString();
+        io.emit("mqttMessage", { topic, msg });
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+});
+
+server.listen(3000);
